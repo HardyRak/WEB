391bb1909aa08c0f13215a3c866bab6eae38cadc
HardyRak <119031361+HardyRak@users.noreply.github.com>
2023-07-07 20:07:46 +0000

    sprint to 5

---
+++ Framework/src/java/etu1986/framework/ModelView.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1986.framework;
+
+/**
+ *
+ * @author hardy
+ */
+public class ModelView {
+    String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
--- Framework/src/java/etu1986/framework/servlet/FrontServlet.java
+++ Framework/src/java/etu1986/framework/servlet/FrontServlet.java
@@ -2,6 +2,7 @@ package etu1986.framework.servlet;
 
 import etu1986.framework.Mapping;
 import etu1986.framework.MethodAnnotation;
+import etu1986.framework.annotation.Url;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -11,11 +12,23 @@ import java.io.File;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class FrontServlet extends HttpServlet {
     RequestDispatcher dispat;
@@ -37,8 +50,9 @@ public class FrontServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         try {
-            this.fillMappingUrls();
-        } catch (ClassNotFoundException e) {
+            String[] packages = this.readConfig();
+            this.fillMappingUrls(packages);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -47,23 +61,46 @@ public class FrontServlet extends HttpServlet {
         this.getDispat().forward(req,resp);
     }
     
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         PrintWriter out = resp.getWriter();
-        String incommingURL = String.valueOf(req.getRequestURL());
-        String target = this.getTarget(incommingURL);
-        System.out.println("URL target >> "+target);
+        String incomingURL = String.valueOf(req.getRequestURL());
+        String target = this.getTarget(incomingURL);
+
+        //System.out.println("Traitement URL");
+
+        Mapping mapping = this.getMappingUrls().get(target);
+        if(mapping != null){
+            //System.out.println("Class: "+mapping.getClassName()+" Method: "+mapping.getMethod());
+            Class<?> clazz = Class.forName(mapping.getClassName());
+            Constructor<?> constructor = clazz.getConstructor();
+            Object clone = constructor.newInstance();
+            Method method = clazz.getMethod(mapping.getMethod());
+
+            String desti = (String) method.invoke(clone);
+            this.redirect(desti, req, resp);
+        }else{
+            //System.out.println("Url inconnu");
+            throw new Exception("Url inconnu");
+        }
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        this.processRequest(req,resp);
+        try {
+            this.processRequest(req,resp);
+        } catch (Exception ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-        this.processRequest(req,resp);
+        try {
+            this.processRequest(req,resp);
+        } catch (Exception ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private String removeHttpProtocoleStr(String URL){
@@ -80,38 +117,73 @@ public class FrontServlet extends HttpServlet {
             return URL.split("/")[1];
         }
     }
-    
-    public List<File> packagesFiles(String[] packages){
+
+    public String[] readConfig() throws IOException, SAXException, ParserConfigurationException {
+        String path = getServletContext().getRealPath("/WEB-INF/myconfig.xml");
+        File inputFile = new File(path);
+        //System.out.println("Absolute path: "+inputFile.getAbsolutePath());
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+
+        NodeList nodeList = doc.getElementsByTagName("myclasspath");
+        List<String> packages = new ArrayList<>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node nNode = nodeList.item(i);
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+                packages.add(element.getElementsByTagName("package").item(0).getTextContent());
+            }
+        }
+
+        String[] list = new String[packages.size()];
+        int it = 0;
+        for(String s : packages){
+            list[it] = s;
+            it++;
+        }
+        return list;
+    }
+    public List<File> packagesFiles(String[] packages) throws Exception {  // String[] to List<String>
         List<File> files = new ArrayList<>();
 
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         for(String pkg : packages){
-            files.add(new File(Objects.requireNonNull(classLoader.getResource(pkg.replace('.', '/'))).getFile()));
+            try{
+                files.add(new File(Objects.requireNonNull(classLoader.getResource(pkg.replace('.', '/'))).getFile()));
+            }catch (Exception e){
+                throw new Exception("Votre package n'existe pas ("+pkg+")");
+            }
         }
         return files;
     }
     
-    private void fillMappingUrls() throws ClassNotFoundException {
+    private void fillMappingUrls(String[] packages) throws Exception {
         this.setMappingUrls(new HashMap<String, Mapping>());
 
-        String[] packages = new String[1];
-        packages[0] = "etu1986.framework.servlet";
         List<File> packageDir = this.packagesFiles(packages);
-
-        for(int i=0 ; i < packages.length ; i++){
+        for(int i=0 ; i < packageDir.size() ; i++){
             if(packageDir.get(i).exists()){
-                String[] files = packageDir.get(i).list();
+                File[] files = packageDir.get(i).listFiles();
                 assert files != null;
-                for(String file : files){
-                    if(file.endsWith(".class")){
-                        String className = file.substring(0, file.length()-6 /* exemple: test.class >> enlever .class */);
-                        Class<?> clazz = Class.forName(packages[i] + '.' + className);
-                        for(Method method : clazz.getDeclaredMethods()){
-                            if(method.isAnnotationPresent(MethodAnnotation.class)){
-                                MethodAnnotation methannot = (MethodAnnotation) method.getAnnotation(MethodAnnotation.class);
-                                this.getMappingUrls().put(methannot.name(), new Mapping(className, method.getName()));
-                                System.out.println("Class: "+className);
-                                System.out.println("Method: "+methannot.name()+" \n");
+                for(File f : files){
+                    String file = f.getName();
+                    if(f.isDirectory()){
+                        String[] newpackage = new String[1];
+                        newpackage[0] = packages[i]+"."+file;
+                        this.fillMappingUrls(newpackage);
+                    }else{
+                        if(file.endsWith(".class")){
+                            String className = file.substring(0, file.length()-6 /* exemple: test.class >> enlever .class */);
+                            Class<?> clazz = Class.forName(packages[i] + '.' + className);
+                            System.out.println(clazz.getName());
+                            for(Method method : clazz.getDeclaredMethods()){
+                                if(method.isAnnotationPresent(Url.class)){
+                                    Url url = (Url) method.getAnnotation(Url.class);
+                                    this.getMappingUrls().put(url.name(), new Mapping(clazz.getName(), method.getName()));
+                                }
                             }
                         }
                     }
